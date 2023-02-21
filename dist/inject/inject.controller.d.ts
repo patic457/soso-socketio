df6570aa7d990941a7d6a42748d593340a47d631
patic457 <patic457@cubes.postbox.in.th>
2023-02-21 09:05:40 +0700

    first

--- dist/inject/inject.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { InjectService } from './inject.service';
-export declare class InjectController {
-    private injectService;
-    constructor(injectService: InjectService);
-    test(): Promise<any>;
-}
--- dist/inject/inject.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { ChatGateway } from '../chat/chat.gateway';
-export declare class InjectService {
-    private chatService;
-    constructor(chatService: ChatGateway);
-    test(): Promise<any>;
-}
